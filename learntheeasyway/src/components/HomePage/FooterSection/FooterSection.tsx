4a1dad5c01cf38ee1ed6f136aaa7c56fbafea0d9
mohinishlokesh96 <65054733+mohinishlokesh96@users.noreply.github.com>
2021-11-26 00:06:57 -0800

    Added midsection and footer section information

---
+++ learntheeasyway/src/components/HomePage/FooterSection/FooterSection.tsx
@@ -0,0 +1,6 @@
+const FooterSection = ()=>{
+    return (
+        <div></div>
+    )
+}
+export default FooterSection
---
+++ learntheeasyway/src/components/HomePage/MidInfoSection/MidInfoSection.tsx
@@ -0,0 +1,6 @@
+const MidInfoSection = ()=>{
+    return (
+        <div></div>
+    )
+}
+export default MidInfoSection
