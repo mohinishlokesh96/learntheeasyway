c1243cb395e350bb65d92e5acb74d1e601f1f9ad
mohinishlokesh96 <65054733+mohinishlokesh96@users.noreply.github.com>
2021-12-23 20:08:59 -0800

    Added footer section css

--- learntheeasyway/src/components/HomePage/FooterSection/FooterSection.tsx
+++ learntheeasyway/src/components/HomePage/FooterSection/FooterSection.tsx
@@ -1,8 +1,9 @@
+import "./FooterSection.css"
 const FooterSection = ()=>{
     return (
         <div className="footer">
             <div>
-                Copyright by Learn The Easy Way 2021
+                <p className="footerInformtaion"><span>&copy;</span>Copyright by Learn The Easy Way 2021</p>
             </div>
         </div>
     )
