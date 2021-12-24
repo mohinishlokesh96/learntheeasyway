06f52b8cccc1b0eace2ca8c39d1383c49372927a
mohinishlokesh96 <65054733+mohinishlokesh96@users.noreply.github.com>
2021-12-23 20:04:52 -0800

    added the min info section details

--- learntheeasyway/src/components/HomePage/MidInfoSection/MidInfoSection.tsx
+++ learntheeasyway/src/components/HomePage/MidInfoSection/MidInfoSection.tsx
@@ -1,9 +1,14 @@
+import "./MidInfoSection.css"
 const MidInfoSection = ()=>{
     return (
         <div>
             <div className="midInfoSection">
-                <div className="midInfoSectionImage"></div>
-                <div className="midInfoSectionInformation"></div>
+                <div className="midInfoSectionImage"> <img src="" alt="Information Section" /></div>
+                <div className="midInfoSectionInformation">
+                    <p>We have a wide variety guides and structure from software interview prepartion and learning new language along with 
+                        career structure. We also provide a wide variety of networking that allows 
+                    </p>
+                </div>
             </div>
         </div>
     )
